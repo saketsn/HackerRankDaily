3a882966ab178a5a2f01dc9cff0e523f3b2bb359
saketsn <saketnandan15@gmail.com>
2025-04-09 01:03:46 +0530

    SinglyLinkedList simple code...

--- SingleLinkedList/Main.java
+++ SingleLinkedList/Main.java
@@ -1,36 +1,61 @@
 package SingleLinkedList;
 
-class Node
-{
+public class Main {
+    static Node head;
+    static Node tail;
+    static int size;
 
-    public int value;
-    public Node next;
-}
-
-
-class SinglyLinkedList{
-
-    public Node head;
-    public Node tail;
-    public int size;
-
-    public Node createSinglylinkedList(int nodeValue){
-        head = new Node();
+    public static void insertInLinkedList(int nodeValue, int location){
         Node node = new Node();
-        node.next = null;
         node.value = nodeValue;
+
+        if(head == null){
+            createSinglylinkedList(nodeValue);
+            return;
+        } else if(location == 0){
+            node.next = head;
+            head = node;
+        } else if(location >= size){
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while(index < location - 1){
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public static void createSinglylinkedList(int nodeValue) {
+        Node node = new Node();
+        node.value = nodeValue;
+        node.next = null;
         head = node;
         tail = node;
         size = 1;
-
-        return head;
     }
-}
 
-class Main{
-    public static void main(String[] args){
-        SinglyLinkedList sll = new SinglyLinkedList();
-        sll.createSinglylinkedList(5);
-        System.out.println(sll.head.value);
+    static class Node {
+        int value;
+        Node next;
+    }
+
+    public static void main(String[] args) {
+        insertInLinkedList(10, 0);
+        insertInLinkedList(20, 1);
+        insertInLinkedList(30, 2);
+        // print list
+        Node current = head;
+        while(current != null){
+            System.out.print(current.value + " -> ");
+            current = current.next;
+        }
     }
 }
