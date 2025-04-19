fc1cea456ec04ab2c4a5b40444ecebc48a7e57b0
saketsn <saketnandan15@gmail.com>
2025-04-19 21:28:57 +0530

    Hollow Rectangle Star Pattern   &  RectangleStarPattern more code added

--- PatternProblem/RectangleStarPattern.java
+++ PatternProblem/RectangleStarPattern.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class RectangleStarPattern {
     public static void main(String[] args) {
 
-        for (int i =0; i<4; i++){
-            for(int j=0; j<5; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for (int i =0; i<4; i++){
+//            for(int j=0; j<5; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number :greater than 4 :\n");
         int num = sc.nextInt();
-        for (int i =0; i<num-1; i++){
-            for(int j=0; j<num; j++){
-                if( i> 0 && i <num-2 && j == 0 || j == num-1) {
-                    System.out.print("*");
-                }else if ( i == 0 || i== num-2){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+//        for (int i =0; i<num-1; i++){
+//            for(int j=0; j<num; j++){
+//                if( i> 0 && i <num-2 && j == 0 || j == num-1) {
+//                    System.out.print("*");
+//                }else if ( i == 0 || i== num-2){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
 //        for (int i =0; i<4; i++){
 //            for(int j=0; j<5; j++){
@@ -39,5 +39,15 @@ public class RectangleStarPattern {
 //            }
 //            System.out.println();
 //        }
+        for (int i =0; i<num-1; i++){
+             for(int k = 0 ; k<=i; k++){
+                 System.out.print(" ");
+             }
+            for(int j=0; j<num; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
