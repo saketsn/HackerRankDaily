baeec220dcfea40184e652d12fbf70f3377c1eb1
saketsn <saketnandan15@gmail.com>
2025-04-19 21:42:16 +0530

    Triangle Star Pattern

---
+++ PatternProblem/TriangleStarPattern.java
@@ -0,0 +1,13 @@
+package PatternProblem;
+
+public class TriangleStarPattern {
+    public static void main(String[] args) {
+
+        for( int i=0; i < 4; i++){
+            for( int j =0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
