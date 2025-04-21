79d2fd442524d98203804f917eb695c76d6c209a
saketsn <saketnandan15@gmail.com>
2025-04-21 20:55:07 +0530

    upload

--- PatternProblem/InvertedPyramidStarPattern.java
+++ PatternProblem/InvertedPyramidStarPattern.java
@@ -1,5 +1,7 @@
 package PatternProblem;
 
+import java.util.Scanner;
+
 public class InvertedPyramidStarPattern {
     public static void main(String[] args) {
         int n=5;
@@ -14,5 +16,35 @@ public class InvertedPyramidStarPattern {
             n= n-2;
             System.out.println();
         }
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Hollow Inverted Pyramid Pattern Rows = ");
+        int rows = sc.nextInt();
+
+        System.out.println("Hollow Inverted Pyramid Star Pattern");
+
+        for (int i = rows ; i > 0; i-- )
+        {
+            for (int j = 1 ; j <= rows - i; j++ )
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1 ; k <= (2 * i) - 1; k++ )
+            {
+                if(i == 1 || i == rows || k == 1 || k == (2 * i) - 1)
+                {
+                    System.out.print("X");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
     }
 }
+
